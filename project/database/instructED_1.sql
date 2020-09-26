332268ec89c655bd700fc85c8d58c84d5a39ce67
shauntrimm <shauntrimm98@gmail.com>
2020-09-26 15:08:45 -0400

    Added phonenumber column in users table

--- project/database/instructED_1.sql
+++ project/database/instructED_1.sql
@@ -4,5 +4,6 @@ create table users (
 	username text,
 	email text not null,
 	pword text not null,
+	phonenumber text not null,	-- For DUO Authentication
 	oauth text,	--stores oauth type ex. ('Google', 'Facebook', null if none)
 	PRIMARY KEY(username));
