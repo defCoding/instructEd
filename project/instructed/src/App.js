d5c493005b236688757329581aaa4360bd2d753d
Andy Jedlicka <andyjedlicka@Andys-MacBook-Pro.local>
2020-09-27 15:47:43 -0400

    updating with new pages

--- project/instructed/src/App.js
+++ project/instructed/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Registration from "./components/registration";
-import Login from "./components/login";
+import Registration from "./registration";
+import Login from "./login";
+
+const PageNotFound = () => (
+  <div>404</div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Login} />
+      <Route path="/registration" component={Registration}
+    </div>
+  </BrowserRouter>
+)
 
 function App() {
   return (
