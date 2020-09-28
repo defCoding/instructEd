72f655fe16f29d928816d55a57b0e029a10880a2
Andy Jedlicka <andyjedlicka@Andys-MacBook-Pro.local>
2020-09-27 20:49:11 -0400

    more updates for structuring

--- project/instructed/src/App.js
+++ project/instructed/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
-import Registration from "./registration";
-import Login from "./login";
-
-const PageNotFound = () => (
-  <div>404</div>
-)
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" component={Login} />
-        <Route path="/registration" component={Registration} />
-      </div>
-    </BrowserRouter>
+    <h1>Hello</h1>
   );
 }
 
