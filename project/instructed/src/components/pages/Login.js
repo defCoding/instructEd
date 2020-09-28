05b4b9966ba5d223ef6f0f82a55d8767bb381a8f
Andy Jedlicka <andyjedlicka@Andys-MacBook-Pro.local>
2020-09-28 15:03:30 -0400

    updates to Login form

--- project/instructed/src/components/pages/Login.js
+++ project/instructed/src/components/pages/Login.js
@@ -5,12 +5,6 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    color: 'gray'
-  },
-});
-
 export class Login extends React.Component {
   state = {
     userName: '',
@@ -31,11 +25,16 @@ export class Login extends React.Component {
     return (
       <>
       <div>
-        <Typography variant="h2">Login</Typography>
+        <Typography 
+          variant="h2">
+            Login
+        </Typography>
           <form>
             <TextField
               name="userName"
               placeholder="Username"
+              variant="outlined"
+              style={styles.TextField}
               value={this.state.userName}
               onChange={this.change} 
             />
@@ -43,17 +42,40 @@ export class Login extends React.Component {
             <TextField 
               name="password"
               type="password"
+              variant="outlined"
+              style={styles.TextField}
               placeholder="Password" 
               value={this.state.password}
               onChange={this.change}  
             />
             <br />
-            <Button size="medium" variant="outlined" onClick={this.onSubmit}>Submit</Button>
+            <Button 
+              size="medium" 
+              variant="outlined" 
+              style={styles.everything}
+              onClick={this.onSubmit}>
+                Submit
+            </Button>
           </form>
-          <Link to="/registration"><Typography variant="overline">Create an account</Typography></Link>
+          <Link 
+            to="/registration">
+              <Typography 
+                variant="overline">
+                  Create an account
+              </Typography>
+          </Link>
       </div>
       </>
     )
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
+  },
+  TextField: {
+    margin: 15
   }
 }
 
