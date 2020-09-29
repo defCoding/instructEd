403011dfad9fb7e1019affa8d851fa1a3fdccd54
Andy Jedlicka <andyjedlicka@Andys-MacBook-Pro.local>
2020-09-29 14:36:04 -0400

    updates to App.css and App.js

--- project/instructed/src/App.js
+++ project/instructed/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/pages/LoginAndRegistration/Login'
 import Registration from './components/pages/LoginAndRegistration/Registration';
+import { makeStyles } from '@material-ui/core';
+import './App.css'
+
+const useStyle = makeStyles(theme => ({
+  background: '#D7D9D7'
+}))
 
 function App() {
+  const classes = useStyle();
   return (
     <>
       <Router>
