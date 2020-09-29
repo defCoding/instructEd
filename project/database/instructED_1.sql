678c67985201ef9e01e1c02842dc68bb38fe1f93
shauntrimm <shauntrimm98@gmail.com>
2020-09-29 14:23:23 -0400

    Updated primary key, and added firstname, and lastname cols

--- project/database/instructED_1.sql
+++ project/database/instructED_1.sql
@@ -1,10 +1,11 @@
 create database instructED_1;
 
 create table users (
-	username varchar(20),
-	email text not null,
+	email text,
+	firstname text,
+	lastname text,
 	pword text not null,
 	phonenumber varchar(10) not null,	-- For DUO Authentication
 	oauth text,	--stores oauth type ex. ('Google', 'Facebook', null if none)
-	PRIMARY KEY(username)
+	PRIMARY KEY(email)
 );
