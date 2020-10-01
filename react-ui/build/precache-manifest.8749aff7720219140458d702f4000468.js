7e4619ff727c83983d01f42ab321ff8cdd157024
Kevin Cao <kcao1998@gmail.com>
2020-09-30 21:20:58 -0400

    compartamentalized front-end and back-end

---
+++ react-ui/build/precache-manifest.8749aff7720219140458d702f4000468.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3060dc1c9fa8fa0b3b54f11ee839fb52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d8c1f71e98f1d2b5a90",
+    "url": "/static/js/2.10a9fec3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.10a9fec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63cfbb8f29b910048bc",
+    "url": "/static/js/main.b8140cb4.chunk.js"
+  },
+  {
+    "revision": "221f99995a4e1481b089",
+    "url": "/static/js/runtime-main.f88c94ec.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const db = require('./queries');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
+console.log("Server started.");
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 app.post('/users', db.createUser);
 
