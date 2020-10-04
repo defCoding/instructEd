a844f0f856f7df36d348898ae467f61d1511f693
Kevin Cao <kcao1998@gmail.com>
2020-10-04 01:03:46 -0400

    another attempt

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get('/dashboard', withAuth, (req, res) => {
 // Catch All
 app.use(express.static(path.join(__dirname, '../react-ui/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../react-ui/build/index.html'), err => {
+  res.sendFile("index.html", path.join(__dirname, '../react-ui/build/'), err => {
 		if (err) {
 			res.status(500).send(err); 
 			});
