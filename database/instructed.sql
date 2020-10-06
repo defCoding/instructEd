1d435177c5b26923ac78d66895fc918a95971737
shauntrimm <shauntrimm98@gmail.com>
2020-10-06 14:27:25 -0400

    Updated database for sprint 2 deliverables

--- database/instructed.sql
+++ database/instructed.sql
@@ -28,3 +28,48 @@ CREATE TABLE FacebookUsers (
   PRIMARY KEY (id),
   FOREIGN KEY (id) REFERENCES Users (id)
 );
+
+CREATE TABLE Courses (
+  course_id VARCHAR(20),
+  course_name VARCHAR(25) NOT NULL,
+  --instructor_id uuid NOT NULL,
+  term_id VARCHAR(20) NOT NULL,
+  PRIMARY KEY (course_id),
+  -- (Depends on future table structure) FOREIGN KEY (instructor_id) REFERENCES Users (id)
+);
+
+CREATE TABLE Enrollments (
+  student_id uuid,
+  course_id VARCHAR(20),
+  PRIMARY KEY (student_id, course_id);
+  FOREIGN KEY (student_id) REFERENCES Users (id),
+  FOREIGN KEY (course_id) REFERENCES Courses (course_id)
+);
+
+CREATE TABLE Instructing (
+  instructor_id uuid,
+  course_id VARCHAR(20),
+  PRIMARY KEY (instructor_id, course_id),
+  FOREIGN KEY (instructor_id) REFERENCES Users (id),
+  FOREIGN KEY (course_id) REFERENCES Courses (course_id)
+);
+
+CREATE TABLE Submissions (
+  submission_id VARCHAR(40), --May need to be changed to a randomly generated id
+  assignment_id VARCHAR(40) NOT NULL, --^^
+  submission_types VARCHAR(10) ARRAY NOT NULL,
+  --content VARCHAR(30) "" ARRAY, "" should be either bytea or large objects, will need a table to store these.
+  grade VARCHAR(5),
+  time_submitted TIMESTAMP,
+  PRIMARY KEY (submission_id),
+  FOREIGN KEY (assignment_id) REFERENCES Assignments (assignment_id)
+);
+
+CREATE TABLE Assignments (
+  assignment_id VARCHAR(40),
+  assignment_name VARCHAR(50) NOT NULL,
+  assignment_decription VARCHAR(500),
+  course_id VARCHAR(20) NOT NULL,
+  deadline TIMESTAMP NOT NULL,
+  submission_types VARCHAR(10) ARRAY NOT NULL,
+);
