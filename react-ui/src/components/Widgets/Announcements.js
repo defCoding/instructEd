9230170403c8bc9e0df640ea36e738bfc029bcb1
shauntrimm <shauntrimm98@gmail.com>
2020-10-12 19:19:28 -0400

    Added initial code for student widgets, and comments for what data is needed

---
+++ react-ui/src/components/Widgets/Announcements.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Announcements extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    //Should return all announcements for courses the student is enrolled in, then in the <div> section display them in a list.
+
+    render() {
+        return (
+            <div>
+                Announcements
+            </div>
+        );    
+    }
+}
--- react-ui/src/components/Widgets/TaskCalendar.js
+++ react-ui/src/components/Widgets/TaskCalendar.js
@@ -7,7 +7,9 @@ class TaskCalendar extends Component {
   }
  
   onChange = date => this.setState({ date })
- 
+  
+  //Need to recieve data on all assignments for each class the student is enrolled in, in which they have not made a submission.
+  //Then in the same division where the calendar is, the assignments for the day toggled by the calendar will be displayed. (using onClickDay() provided by the Calendar object)
   render() {
     return (
       <div>
