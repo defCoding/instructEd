ff7b63c52b0a7e87561ab5ec50a5f47fa6704527
shauntrimm <shauntrimm98@gmail.com>
2020-10-13 15:40:47 -0400

    Changed comment on UpcomingAssignments.js

--- react-ui/src/components/Widgets/UpcomingAssignments.js
+++ react-ui/src/components/Widgets/UpcomingAssignments.js
@@ -5,7 +5,7 @@ class UpcomingAssignments extends Component {
         super(props);
     }
 
-    //Should return all announcements for courses the student is enrolled in, then in the <div> section display them in a list.
+    //Should return all assignments for courses the student is enrolled in, then in the <div> section display them in a list.
     render() {
         const data = [{"Assignment 1":"This is assignment 1"}, {"Assignment 2":"This is assignment 2"}];
         const listitems = data.map((a) => <li key={a.name}>{a.name}</li>);
