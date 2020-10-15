12ca4f3446bc7734ab92c41893478ca2f5918614
Andrew Jedlicka <53351246+ajedlicka@users.noreply.github.com>
2020-10-15 14:01:45 -0400

    Update index.js

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
