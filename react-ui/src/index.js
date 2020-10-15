48eea3e8fdadf54d234afff94e64b9eb37de4ce1
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-10-15 13:53:18 -0400

    updating theme

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#b9e5f3',
+    },
+    secondary: {
+      main: '#8a140e',
+    },
+    type: 'dark',
+  }
+});
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
