297591a1b9f8198ff71147b0bd2b37e671af9776
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-10-15 14:12:28 -0400

    updating themes

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,35 +6,21 @@ import ForgotPassword from './components/LoginAndRegistration/ForgotPassword';
 import ResetPassword from './components/LoginAndRegistration/ResetPassword';
 import DuoLogin from './components/LoginAndRegistration/DuoLogin';
 import Dashboard from './components/Dashboard/Dashboard';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#2E4057'
-    },
-    secondary: {
-      main:'#ffffff'
-    }
-  }
-});
 
 function App() {
   return (
     <>
-      <MuiThemeProvider theme={theme}>
-        <Router>
-            <Switch>
-              <Route path="/" exact component={Login} />
-              <Route path="/login" exact component={Login} />
-              <Route path='/registration' component={Registration} />
-              <Route path='/forgotpassword' component={ForgotPassword} />
-              <Route path='/resetpassword/:token' component={ResetPassword} />
-              <Route path='/dashboard' component={Dashboard} />
-              <Route path='/duologin' component={DuoLogin} />
-            </Switch>
-          </Router>
-      </MuiThemeProvider>
+      <Router>
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/login" exact component={Login} />
+            <Route path='/registration' component={Registration} />
+            <Route path='/forgotpassword' component={ForgotPassword} />
+            <Route path='/resetpassword/:token' component={ResetPassword} />
+            <Route path='/dashboard' component={Dashboard} />
+            <Route path='/duologin' component={DuoLogin} />
+          </Switch>
+        </Router>
     </>
   );
 }
