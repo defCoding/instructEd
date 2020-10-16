213fb335f05939b841f97732fede5a13556d86b8
shauntrimm <shauntrimm98@gmail.com>
2020-10-16 13:25:10 -0400

    Updated to function based components for Announcements.js and UpcomingAssignments.js

--- react-ui/src/components/Widgets/Announcements.js
+++ react-ui/src/components/Widgets/Announcements.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Announcements extends Component {
-    constructor(props){
-        super(props);
-    }
-
+export default function Announcements() {
+    const data = [{"Announcement 1":"This is announcement 1"}, {"Announcement 2":"This is announcement 2"}];
+    const listitems = data.map((a) => <li key={a.name}>{a.name}</li>);
     //Should return all announcements for courses the student is enrolled in, then in the <div> section display them in a list.
 
-    render() {
-        const data = [{"Announcement 1":"This is announcement 1"}, {"Announcement 2":"This is announcement 2"}];
-        const listitems = data.map((a) => <li key={a.name}>{a.name}</li>);
-        return (
-            <div>
-                {listitems}
-            </div>
-        );    
-    }
+    return (
+        <div>
+            {listitems}
+        </div>
+    );    
+    
 }
--- react-ui/src/components/Widgets/UpcomingAssignments.js
+++ react-ui/src/components/Widgets/UpcomingAssignments.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class UpcomingAssignments extends Component {
-    constructor(props){
-        super(props);
-    }
-
+export default function Announcements() {
+    const data = [{"Announcement 1":"This is announcement 1"}, {"Announcement 2":"This is announcement 2"}];
+    const listitems = data.map((a) => <li key={a.name}>{a.name}</li>);
     //Should return all assignments for courses the student is enrolled in, then in the <div> section display them in a list.
-    render() {
-        const data = [{"Assignment 1":"This is assignment 1"}, {"Assignment 2":"This is assignment 2"}];
-        const listitems = data.map((a) => <li key={a.name}>{a.name}</li>);
-        return (
-            <div>
-                {listitems}
-            </div>
-        );    
-    }
+
+    return (
+        <div>
+            {listitems}
+        </div>
+    );    
+    
 }
