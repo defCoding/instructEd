72258fdc94f3704f9932c4b009d60c4f42e38bb0
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-10-16 13:35:55 -0400

    updating announcements

--- react-ui/src/components/Widgets/Announcements.js
+++ react-ui/src/components/Widgets/Announcements.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { List, ListItemText, ListItem, Divider } from '@material-ui/core';
 
@@ -17,19 +16,4 @@ export default function Announcements() {
             ))}
         </List>
     );
-=======
-import React, { useState } from 'react';
-
-export default function Announcements() {
-    const data = [{"Announcement 1":"This is announcement 1"}, {"Announcement 2":"This is announcement 2"}];
-    const listitems = data.map((a) => <li key={a.name}>{a.name}</li>);
-    //Should return all announcements for courses the student is enrolled in, then in the <div> section display them in a list.
-
-    return (
-        <div>
-            {listitems}
-        </div>
-    );    
-    
->>>>>>> 213fb335f05939b841f97732fede5a13556d86b8
 }
