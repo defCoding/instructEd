c79312cdbfa9a91ae5e7b08176d8eca9e4389048
shauntrimm <shauntrimm98@gmail.com>
2020-10-16 14:49:17 -0400

    Updated Announcements.js

--- react-ui/src/components/Widgets/Announcements.js
+++ react-ui/src/components/Widgets/Announcements.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { List, ListItemText, ListItem, Divider } from '@material-ui/core';
 
-const data = [{"header":"P465", "body":"This is announcement 1"}, {"header":"P465", "body":"This is announcement 2"}];
+//const announcements = [{"header":"P465", "body":"This is announcement 1"}, {"header":"P465", "body":"This is announcement 2"}];
 
 export default function Announcements() {
+    const [announcements, setAnnouncements] = useState([]);
+    setAnnouncements();
     return (
         <List>
-            {data.map((announcement) => (
+            {announcements.map((announcement) => (
                 <>
                     <ListItem>
-                        <ListItemText primary={announcement.header} secondary={announcement.body} />
+                        <ListItemText primary={announcement.announcement_title} secondary={announcement.course_id} />
                     </ListItem>
                     <Divider />
                 </>
             ))}
         </List>
     );
+
+    function setAnnouncements(){
+        announcements = axios.get("/student/announcements");
+    }
 }
