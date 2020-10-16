24cfd9c038611768667aa6ce2575d7667c0b4c93
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-10-16 14:28:08 -0400

    working on addcourse

--- react-ui/src/components/Widgets/AddCourse.js
+++ react-ui/src/components/Widgets/AddCourse.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { List, ListItemText, ListItem, Divider } from '@material-ui/core';
+import { Paper, TextField, Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const data = [{"header":"P465", "body":"This is announcement 1"}, {"header":"P465", "body":"This is announcement 2"}];
+const classFields = {
+  className: '',
+  department: '',
+  classNumber: '',
+  instructor: '',
+}
+
+const useStyle = makeStyles(theme => ({
+  items: {
+    margin:theme.spacing(1)
+  },
+}))
 
 export default function Announcements() {
+  const classes = useStyle();
+
     return (
-        <List>
-            {data.map((announcement) => (
-                <>
-                    <ListItem>
-                        <ListItemText primary={announcement.header} secondary={announcement.body} />
-                    </ListItem>
-                    <Divider />
-                </>
-            ))}
-        </List>
+      <Paper>
+        <form>
+          <Grid height="100%" spacing={1}>
+            <Grid item xs="12">
+              <TextField color="secondary" variant="outlined" label="Class Name" name="className" className={classes.items} />
+            </Grid>
+            <Grid item xs="12">
+              <TextField color="secondary" variant="outlined" label="Class Number" name="classNumber" className={classes.items} />
+            </Grid>
+            <Grid item xs="12">
+              <TextField color="secondary" variant="outlined" label="Department" name="department" className={classes.items} />
+            </Grid>
+            <Grid item xs="12">
+              <TextField color="secondary" variant="outlined" label="Instructor" name="instructor" className={classes.items} />
+            </Grid>
+            <Grid item xs="12">
+              <Button variant="contained" color="secondary" className={classes.items} >Add Course </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
     );
 }
