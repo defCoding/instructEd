2a4075f3d199b52d860b79def010dd28ee92da4d
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-10-26 23:47:05 -0400

    Organizing Classview

---
+++ react-ui/src/components/Dashboard/ClassViews/AssignmentPanel.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Tabs, Tab, List, ListItemText, Paper, IconButton, Drawer, Divider, ListItem, Typography, Dialog, AppBar, Toolbar } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: theme.palette.secondary.main,
+  },
+  appBar: {
+    position: 'relative',
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  dialog: {
+    padding: theme.spacing(3),
+    height: "100vh",
+  }
+}));
+
+function AssignmentPanel(props) {
+  const classes = useStyles();
+  const assignments = props.assignments;
+
+  return (
+    <Paper className={classes.dialog}>
+      <Typography variant="h6">
+        Assignments
+    </Typography>
+      <List>
+        {
+        assignments.map(assignment => {
+          let date = moment(assignment.deadline).local();
+          date = date.format('[Due on] MM-DD-YY [at] h:mm A');
+
+          return (
+            <ListItem>
+              <ListItemText primary={assignment.assignment_name} secondary={date} />
+            </ListItem>
+          );
+        })
+        }
+      </List>
+    </Paper>
+  );
+}
---
+++ react-ui/src/components/Dashboard/ClassViews/HomePanel.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Tabs, Tab, List, ListItemText, Paper, IconButton, Drawer, Divider, ListItem, Typography, Dialog, AppBar, Toolbar } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: theme.palette.secondary.main,
+  },
+  appBar: {
+    position: 'relative',
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  dialog: {
+    padding: theme.spacing(3),
+    height: "100vh",
+  }
+}));
+
+function GeneralPanel(props) {
+  const classes = useStyles();
+  const courseData = props.courseData;
+  return (
+    <Paper className={classes.dialog}>
+      <Typography variant="h5">
+        Course Information
+      </Typography>
+      <Typography variant="h6">
+        Term: {courseData.term}
+      </Typography>
+      <Typography variant="h6">
+        Instructors
+        <List>
+          {
+            courseData.instructors.map(instructor => {              
+              return (
+                <ListItem>
+                  <ListItemText primary={instructor} />
+                </ListItem>
+              )
+            })
+          }
+        </List>
+      </Typography>
+    </Paper>
+  );
+}
