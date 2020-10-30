d319682ac67f5e0ea6bbdbc28cbb9d30d41cb046
shauntrimm <shauntrimm98@gmail.com>
2020-10-30 14:24:56 -0400

    Added dialog function to UnapprovedFiles.js

--- react-ui/src/components/Widgets/UnapprovedFiles.js
+++ react-ui/src/components/Widgets/UnapprovedFiles.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { List, ListItemText, ListItemSecondaryAction, ListItem, Divider } from '@material-ui/core'
+import { Button, List, ListItemText, ListItemSecondaryAction, ListItem, Divider, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core'
 
 export default function UnapprovedFiles(){
-    const [files, setFiles] = useState([])
+    const [files, setFiles] = useState(["File"]) //Will be [] after testing
+    const [open, setOpen] = React.useState(false);
     useEffect(() => {
         //Place for get request
     });
@@ -18,26 +19,83 @@ export default function UnapprovedFiles(){
         //If not a video file then download the file for viewing
     }
 
-    function fileListItemClicked(){
+    function fileListItemClicked(filelistitem){
         //Bring up dialog that gives the option to approve/disapprove of the file's upload
+        setOpen(true)
+        setFile(filelistitem)
     }
 
     return (
-        <List>
-            {files.map((file) =>
-                <>
-                    <ListItem onClick={fileListItemClicked} button={true}>
-                        <ListItemText />
-                        <ListItemSecondaryAction>
-                            <Button onClick={viewBtnClicked} variant="contained" color="primary">
-                                View
-                            </Button>
-                        </ListItemSecondaryAction>
+        <div>
+            <List>
+                {files.map((file) =>
+                    <>
+                        <ListItem onClick={fileListItemClicked} button={true}>
+                            <ListItemText primary={file} />
+                            <ListItemSecondaryAction>
+                                <Button onClick={viewBtnClicked} variant="contained" color="primary">
+                                    View
+                                </Button>
+                            </ListItemSecondaryAction>
 
-                    </ListItem>
-                    <Divider />
-                </>
-            )}
-        </List>
+                        </ListItem>
+                        <Divider />
+                    </>
+                )}
+            </List>
+            <ApprovalDialog selectedFile={file} open={open} setOpen={setOpen}/>
+        </div>
     );
+}
+
+function ApprovalDialog(selectedFile, open, setOpen){
+    approved = null;
+
+    useEffect(() => {
+        //check if selected file is null
+        if(selectedFile == null){}
+    });
+
+  const handleClose = () => {  
+    setOpen(false);
+  };
+
+  function approveClicked(){
+      approved = true;
+      //handle approval of file upload then close
+      handleClose();
+
+  }
+  function disapproveClicked(){
+      approved = false;
+      //handle disapproval of file upload then close
+      handleClose();
+  }
+
+  return (
+    <Dialog
+        open = {open}
+        onClose = {handleClose}
+        aria-labelled-by="approve-dialog-title"
+        aria-described-by="approve-dialog-filename" //will be filled by actual filename variable
+    >
+        <DialogTitle id="approve-dialog-title">{Approve/Disapprove}</DialogTitle>
+        <DialogContent>
+            <DialogContentText id="approve-dialog-filename">
+                FileName
+            </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={approveClicked} color="primary">
+                Approve
+            </Button>
+            <Button onClick={disapproveClicked} color="primary">
+                Disapprove
+            </Button>
+            <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+        </DialogActions>
+    </Dialog>
+  );
 }
