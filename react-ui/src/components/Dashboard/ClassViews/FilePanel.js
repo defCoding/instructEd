9fde1b49cc214f3c60e3ef5419c6d5f6d3011735
shauntrimm <shauntrimm98@gmail.com>
2020-10-31 17:47:13 -0400

    Added role checks and variables for FilePanel.js and LecturePanel.js

--- react-ui/src/components/Dashboard/ClassViews/FilePanel.js
+++ react-ui/src/components/Dashboard/ClassViews/FilePanel.js
@@ -29,6 +29,12 @@ export default function FilePanel() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [selectedFile, setSelectedFile] = React.useState(null);
+  var role = 0; //0 for non-instructor, 1 for instructor
+
+  useEffect(() => {
+    //Determine the role for the given class
+    //If instructor set role to 1, if admin or student set role to 0
+  });
 
   return (
     <>
