367777f8ea058cfe86d2a3d254ffca3d6ac77358
shauntrimm <shauntrimm98@gmail.com>
2020-10-31 20:00:53 -0400

    Edit FileUpload.js

--- react-ui/src/components/Dashboard/ClassViews/FileUpload.js
+++ react-ui/src/components/Dashboard/ClassViews/FileUpload.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
 
-export default function FileUpload(){
+export default function FileUpload({open, setOpen}){
   const [file, setFile] = useState(null);
+
+  const handleClose = () => {  
+    setOpen(false);
+  };
 
   const submitFile = async () => {
     try {
@@ -23,10 +28,36 @@ export default function FileUpload(){
   };
 
   return (
-    <form onSubmit={submitFile}>
-      <label>Upload file</label>
-      <input type="file" onChange={event => setFile(event.target.files)} />
-      <button type="submit">Send</button>
-    </form>
+    <Dialog
+        open = {open}
+        onClose = {handleClose}
+        aria-labelled-by="approve-dialog-title"
+        aria-described-by="approve-dialog-filename" //will be filled by actual filename variable
+    >
+        <DialogTitle id="approve-dialog-title">{"Upload File"}</DialogTitle>
+        <DialogContent>
+        <TextField
+          name="upload-file"
+          type="file"
+          onChange={event => setFile(event.target.files)}
+        />
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={submitFile} color="primary">
+                Upload
+            </Button>
+            <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+        </DialogActions>
+    </Dialog>
   );
-};
+
+ // return (
+ //   <form onSubmit={submitFile}>
+ //     <label>Upload file</label>
+ //     <input type="file" onChange={event => setFile(event.target.files)} />
+ //     <button type="submit">Send</button>
+ //   </form>
+ // );
+}
