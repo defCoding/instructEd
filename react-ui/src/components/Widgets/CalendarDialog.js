9b60a9c6c7cb0683c81ee54c1148e126c1971625
Kevin Cao <kcao1998@gmail.com>
2020-11-03 15:02:13 -0500

    removed useStyles from import

--- react-ui/src/components/Widgets/CalendarDialog.js
+++ react-ui/src/components/Widgets/CalendarDialog.js
@@ -1,4 +1,4 @@
-import React, { useStyle } from 'react';
+import React, { } from 'react';
 import Calendar from 'react-calendar';
 import { Button, Dialog, DialogActions, DialogTitle, DialogContent, TextField } from '@material-ui/core';
 
