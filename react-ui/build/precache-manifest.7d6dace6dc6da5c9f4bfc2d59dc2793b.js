ccb5ed8a8e6ee0d593be0a75e4422e59d6751c3f
shauntrimm <shauntrimm98@gmail.com>
2020-11-15 17:42:51 -0500

    Updated Grading and UnapprovedFiles.js

---
+++ react-ui/build/precache-manifest.7d6dace6dc6da5c9f4bfc2d59dc2793b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02a05fd3bdfe23dea9f7c070e7996ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27ea8c72d05d5c79e3b",
+    "url": "/static/js/2.ebf8e666.chunk.js"
+  },
+  {
+    "revision": "7cdb8ac460e2731bc8fc9421c3ad27c1",
+    "url": "/static/js/2.ebf8e666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527e6448d1c5e9db66e5",
+    "url": "/static/js/main.0f527946.chunk.js"
+  },
+  {
+    "revision": "221f99995a4e1481b089",
+    "url": "/static/js/runtime-main.f88c94ec.js"
+  }
+]);
