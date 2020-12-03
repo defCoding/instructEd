40525cb960a0222d41cb16dff6765324aa9552cc
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-12-03 13:36:04 -0500

    updates to landing page

--- react-ui/src/components/LoginAndRegistration/LandingPage/LandingPage.js
+++ react-ui/src/components/LoginAndRegistration/LandingPage/LandingPage.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from './Header';
-import LandingPic from './landing.jpg'
+import LandingPic from './landing.jpg';
+import LandingDark from './landing_dark.jpg';
+
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
-    backgroundImage: 'url(' + LandingPic + ')',
+    backgroundImage: 'url(' + LandingDark + ')',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
