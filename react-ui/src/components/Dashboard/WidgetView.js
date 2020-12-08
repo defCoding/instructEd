7dfbe39c2dc8a25a51d739ca57682b2cef982071
Andrew Jedlicka <andy.j.1999@gmail.com>
2020-12-08 15:07:27 -0500

    updates for photo dashboard

--- react-ui/src/components/Dashboard/WidgetView.js
+++ react-ui/src/components/Dashboard/WidgetView.js
@@ -2,7 +2,8 @@ import React from 'react';
 import WidgetCase from './WidgetCase';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
-import LightBackground from './assets/background.jpg';
+import LightBackground from './assets/light.jpg';
+import DarkBackground from './assets/dark.png';
 
 const useStyles = makeStyles((theme) => ({
   // necessary for content to be below app bar
@@ -11,18 +12,18 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
     minHeight: '100vh',
-    backgroundImage: 'url(' + LightBackground + ')',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
 }));
 
 export default function WidgetView(props) {
+  const darkState = props.darkState;
   const classes = useStyles();
   const currentWidgets = props.displayWidgets;
   
   return(
-    <main className={classes.content}>
+    <main className={classes.content} style={{ backgroundImage: (darkState ? ('url(' + DarkBackground + ')') : ('url(' + LightBackground + ')'))}} >
       <div className={classes.toolbar} />
       <Grid container height="100%" spacing={1}>
         <Grid item sm={12} md={6} maxHeight={300}>
